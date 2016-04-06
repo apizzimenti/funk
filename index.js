ec61bfc4577c21864a57c26ce0c6630248ccebe5
apizzimenti <pizzimentianthony@gmail.com>
2016-04-05 22:21:59 -0500

    add groupByStep(), docs

--- index.js
+++ index.js
@@ -112,3 +112,63 @@ var removeValue = function (value) {
 };
 
 Array.prototype.removeValue = removeValue;
+
+/**
+ * @summary Groups the values of the array by step; groups array values into groups of size step.
+ *
+ * @param step {Number} size of group
+ *
+ * @this {Array} current Array object
+ *
+ * @returns {Array}
+ *
+ * @since 1.0.5
+ *
+ * @example
+ * var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ * a.groupByStep(3)
+ * // [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [10] ]
+ *
+ * var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+ *     c = b.groupByStep(2)
+ * // [ [1, 2], [3, 4], [5, 6], [7, 8], [9, 10] ]
+ */
+
+var groupByStep = function (step) {
+
+    var right = this.length % step,
+        last_length = this.length - right,
+        step_array = [],
+        last_array = [],
+        index = 0;
+
+    for (var i = 0; i < this.length - right; i++) {
+
+        step_array.push(this[i]);
+        index = Math.floor((i + 1) / step) - 1;
+
+        if ((i + 1) % step === 0) {
+
+            this[index] = step_array;
+            step_array = [];
+        }
+    }
+
+    for (var j = this.length - right; j < this.length; j++) {
+        last_array.push(this[j]);
+        delete this[j];
+    }
+
+    this.length = last_length;
+
+    for (var k = last_length - last_array.length; k < last_length; k++) {
+        delete this[k];
+    }
+
+    this.length = this.length / step;
+    this.push(last_array);
+
+    return this;
+};
+
+Array.prototype.groupByStep = groupByStep;
