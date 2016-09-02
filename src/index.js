0946ae111023c7ae26c58d7f82f933e3e927ec78
apizzimenti <pizzimentianthony@gmail.com>
2016-09-02 01:44:17 -0500

    update parent object

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@
 /**
  * @author Anthony Pizzimenti
  *
- * @desc Parent object for all funk methods.
+ * @desc Parent object for all funk methods. Each time a method is run, funk keeps a reference to the last array it was
+ * fed before it was modified.
  *
- * @property removeValue {function} Removes the specified value (or list of values) from the specified array.
+ * @property lastArray {*[]} A copy of the last array modified by funk.
  *
  * @type {{}}
  * @global
  */
 
-var f = {};
+var f = {
+    lastArray: []
+};
